e0a2a8dc490e7c69d997ac3f622c2bef1d914c3c
David Ivanov <david.ivanov10@gmail.com>
2026-06-18 20:14:45 +0200

    feat: add solution for nucleotide-count

---
+++ exercism/nucleotide-count/nucleotide_count.cpp
@@ -0,0 +1,18 @@
+#include "nucleotide_count.h"
+
+#include <stdexcept>
+
+namespace nucleotide_count {
+
+std::map<char, int> count(const std::string_view dna_sequence) {
+  std::map<char, int> result{{'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}};
+  for (char c : dna_sequence) {
+    auto it = result.find(c);
+    if (it == result.end()) {
+      throw std::invalid_argument("Invalid nucleotide found in sequence");
+    }
+    it->second++;
+  }
+  return result;
+}
+}  // namespace nucleotide_count
